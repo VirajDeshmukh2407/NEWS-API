37d9a162a418cd0123bbed9873376f18c9d1fdf7
viraj deshmukh <128155984+VirajDeshmukh2407@users.noreply.github.com>
2024-07-23 18:02:07 +0530

    index.js updated

--- index.js
+++ index.js
@@ -7,16 +7,29 @@ const PORT = process.env.PORT || 5000;
 const API_KEY = "19e370ed9a484826a1100e7436f463fe";
 const BASE_URL = "https://newsapi.org/v2/top-headlines";
 
-// app.use(cors());
-
 app.use(
   cors({
     origin: "*",
     methods: ["GET"],
   })
 );
+app.use(express.json());
+
 app.get("/api/news", async (req, res) => {
-  const url = `${BASE_URL}?country=in&apiKey=${API_KEY}`;
+  const category = req.query.category || "general";
+  const url = `${BASE_URL}?country=in&category=${category}&apiKey=${API_KEY}`;
+
+  try {
+    const response = await axios.get(url);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).send("Error fetching news data");
+  }
+});
+
+app.post("/api/news", async (req, res) => {
+  const category = req.body.category || "general";
+  const url = `${BASE_URL}?country=in&category=${category}&apiKey=${API_KEY}`;
 
   try {
     const response = await axios.get(url);
@@ -29,59 +42,3 @@ app.get("/api/news", async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-// netlify/functions/fetch-news.js
-
-// const axios = require("axios");
-
-// exports.handler = async function (event, context) {
-//   const my_api_key = process.env.REACT_APP_NEWS_API_KEY;
-//   const url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${my_api_key}`;
-//   try {
-//     const response = await axios.get(
-//       // `https://newsapi.org/v2/top-headlines?country=in&apiKey=${url}`,
-//       url
-//       // console.log(process.env.REACT_APP_NEWS_API_KEY)
-//     );
-//     return {
-//       headers: {
-//         "Access-Control-Allow-Origin": "*",
-//         "Access-Control-Allow-Headers": "Content-Type",
-//       },
-//       statusCode: 200,
-//       body: JSON.stringify(response.data),
-//     };
-//   } catch (error) {
-//     return {
-//       statusCode: error.response.status,
-//       body: JSON.stringify({ message: error.message }),
-//     };
-//   }
-// };
-
-// const axios = require("axios");
-// const cors = require("cors")({ origin: true });
-
-// exports.handler = async function (event, context, callback) {
-//   const my_api_key = process.env.REACT_APP_NEWS_API_KEY;
-//   const url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${my_api_key}`;
-
-//   cors(event, context, async () => {
-//     try {
-//       const response = await axios.get(url);
-//       callback(null, {
-//         statusCode: 200,
-//         headers: {
-//           "Access-Control-Allow-Origin": "*",
-//           "Access-Control-Allow-Headers": "Content-Type",
-//         },
-//         body: JSON.stringify(response.data),
-//       });
-//     } catch (error) {
-//       callback(null, {
-//         statusCode: error.response.status,
-//         body: JSON.stringify({ message: error.message }),
-//       });
-//     }
-//   });
-// };
