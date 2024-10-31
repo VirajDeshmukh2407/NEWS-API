627d565439a1c8c19eb73c2d9001818d55fd6328
viraj deshmukh <128155984+VirajDeshmukh2407@users.noreply.github.com>
2024-10-31 14:38:36 +0530

    API CHANGED

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 9000;
 
 // const API_KEY = "19e370ed9a484826a1100e7436f463fe";
 const API_KEY = "2BnUs6BowuC5s8EU6p9bLnhMpY8TleVAUO8RH_QfN2niwr60";
 // const BASE_URL = "https://newsapi.org/v2/top-headlines";
-const BASE_URL = "'https://api.currentsapi.services/v1/latest-news?'";
+const BASE_URL = "https://api.currentsapi.services/v1/latest-news";
 
 app.use(
   cors({
@@ -20,6 +20,7 @@ app.use(express.json());
 app.get("/api/news", async (req, res) => {
   const category = req.query.category || "general";
   const url = `${BASE_URL}?country=in&category=${category}&apiKey=${API_KEY}`;
+  // const url = `${BASE_URL}?country=in&category=${category}&apiKey=${API_KEY}`;
 
   try {
     const response = await axios.get(url);
